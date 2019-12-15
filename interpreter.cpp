074c6223d50fd98d8e88730877812dcc45c367ce
shalini <shalinisaiprasad@gmail.com>
2019-12-15 08:51:47 +0530

    select done

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,7 +1,12 @@
 #include "interpreter.h"
-#include <cstring>
+#include <string>
 
-table::table(vector<string> column_names)
+Table::Table()
+{
+
+}
+
+Table::Table(vector<string> column_names)
 {
 	for(auto name : column_names)
 	{
@@ -9,7 +14,7 @@ table::table(vector<string> column_names)
 	}
 }
 
-void table::add_row(map<string, string> row)
+void Table::add_row(map<string, string> row)
 {
 	for(auto r: row)
 	{
@@ -17,7 +22,7 @@ void table::add_row(map<string, string> row)
 	}
 }
 
-void table::display()//change to make it display like a table
+void Table::display()//change to make it display like a table
 {
 	cout<<endl;
 	for(auto col : t)
@@ -30,9 +35,7 @@ void table::display()//change to make it display like a table
 	cout<< '\n';
 }
 
-
-
-void table::del_row(int row_num)//delete does not work. have to use references??
+void Table::del_row(int row_num)//delete does not work. have to use references??
 {
 
 	for(auto col: t)
@@ -58,67 +61,34 @@ void table::del_row(int row_num)//delete does not work. have to use references??
 	}
 }
 
-table::~table()
+vector<string> Table::sel_row(int row_num)//returns row as vector of strings
 {
-
+	vector<string> row;
+	for(auto col: t)
+	{
+		vector<string>::iterator ite = col.second.begin()+row_num;
+		row.push_back(*ite);
+	}
+	return row;
 }
 
-void table::interpret(char query[])
+Table::Table(const Table& rhs)
 {
-
-	cout<<query<<endl;
-	vector<string> tokens;
-	char *word = strtok(query, " ");
-
-	while (word != NULL)
-	{
-		tokens.push_back(word);
-		word = strtok(NULL, " ");
-	}
-	string first = *tokens.begin();
-
-	if(strcmp("SELECT",first.c_str())==0)
-	{
-	  selectfrom(tokens[1]);
-	}
-	if(strcmp("DELETE",first.c_str())==0)
-	{
-	  deletewhere(tokens[4],tokens[6]);
-	}
-
+	t = rhs.t;
 }
 
-void table::selectfrom (string colname)
+Table& Table::operator=(const Table& rhs)
 {
-	cout<<colname<<" : ";
-	for(auto col : t)
+	if(this != &rhs)
 	{
-		if( strcmp(col.first.c_str(),colname.c_str())==0)
-		{
-			for(auto row : col.second)
-				cout << row << " ";
-			cout << "\n\n";
-		}
+		t = rhs.t;
 	}
+	return *this;
 }
 
-void table::deletewhere(string colname,string val)
+Table::~Table()
 {
-		for(auto col: t)
-		{
-				if( strcmp(col.first.c_str(),colname.c_str())==0)
-				{
-					int x= 0;
-					for(	vector<string>::iterator ite = col.second.begin(); ite!=col.second.end();++ite,++x)
-					{
-					//	cout<<(*ite).c_str();
-						if(strcmp((*ite).c_str(),val.c_str())==0)
-						{
-								del_row(x);
-						}
-					}
-				}
-		}
+
 }
 
 Context::Context()
@@ -126,7 +96,151 @@ Context::Context()
 
 }
 
+void Context::set_table(string table_name)
+{
+	table = table_name;
+	// for(auto t : tables)
+	// {
+	// 	if(t.first == table_name)
+	// 	{
+	// 		table = tables[t.first];
+	// 		break;
+	// 	}
+	// }
+}
+
+void Context::set_column(string column_name)
+{
+	column = column_name;
+	// for(auto col: table.t)
+	// {
+	// 	if(strcmp(col.first, column_name)==0)
+	// 	{
+	// 		column = col.second;
+	// 		break;
+	// 	}
+	// }
+}
+
+void Context::set_filter(function<bool(string, string)> predicate)
+{
+	pred = predicate;
+}
+
+void Context::clear()
+{
+	table = "";
+	column = "";
+	pred = nullptr;
+}
+
+Table Context::get_table()
+{
+	Table current_table;
+	for(auto tab : tables)
+	{
+		if(tab.first == table)
+		{
+			return tab.second;
+		}
+	}
+}
+
+
+vector<string> Context::get_column()
+{
+	Table current_table = get_table();
+	vector<string> current_column;
+
+	for(auto col: current_table.t)
+	{
+		if(col.first == column)
+		{
+			current_column = col.second;
+			break;
+		}
+	}
+
+}
+
+
+vector<vector<string>> Context::search_on_filter(function<bool(string)> pred) //Return indices - rows where filter is true. Needs to be passed to sel_row to return the row
+{
+	vector<string> column = get_column();
+	vector<int> indices;
+	for (auto it = find_if(column.begin(), column.end(), pred); it != column.end(); it = find_if(++it, column.end(), pred))
+	{
+	    indices.push_back(it - column.begin());
+	}
+	Table table = get_table();
+	vector<vector<string>> selected_rows;
+	for(auto i : indices)
+		selected_rows.push_back(table.sel_row(i));
+	return selected_rows;
+}
+
+// void delete_on_filter(function<bool(string, string)>);
+
 Context::~Context()
 {
 
 }
+// void table::interpret(char query[])
+// {
+
+// 	cout<<query<<endl;
+// 	vector<string> tokens;
+// 	char *word = strtok(query, " ");
+
+// 	while (word != NULL)
+// 	{
+// 		tokens.push_back(word);
+// 		word = strtok(NULL, " ");
+// 	}
+// 	string first = *tokens.begin();
+
+// 	if(strcmp("SELECT",first.c_str())==0)
+// 	{
+// 	  selectfrom(tokens[1]);
+// 	}
+// 	if(strcmp("DELETE",first.c_str())==0)
+// 	{
+// 	  deletewhere(tokens[4],tokens[6]);
+// 	}
+
+// }
+
+// void table::selectfrom (string colname)
+// {
+// 	cout<<colname<<" : ";
+// 	for(auto col : t)
+// 	{
+// 		if( strcmp(col.first.c_str(),colname.c_str())==0)
+// 		{
+// 			for(auto row : col.second)
+// 				cout << row << " ";
+// 			cout << "\n\n";
+// 		}
+// 	}
+// }
+
+// void table::deletewhere(string colname,string val)
+// {
+// 		for(auto col: t)
+// 		{
+// 				if( strcmp(col.first.c_str(),colname.c_str())==0)
+// 				{
+// 					int x= 0;
+// 					for(	vector<string>::iterator ite = col.second.begin(); ite!=col.second.end();++ite,++x)
+// 					{
+// 					//	cout<<(*ite).c_str();
+// 						if(strcmp((*ite).c_str(),val.c_str())==0)
+// 						{
+// 								del_row(x);
+// 						}
+// 					}
+// 				}
+// 		}
+// }
+
+
--- interpreter.h
+++ interpreter.h
@@ -1,75 +1,82 @@
 /*Context contains the tables. Tables is a map of table name to table. Table is a map of column names to a vector of strings.*/
 #ifndef INTERPRETER
-#define INTERPRETER
+#define INTERPRETER 
 
 #include <iostream>
 #include <vector>
 #include <map>
 #include <string>
+#include <functional>
+#include <algorithm>
 #include <cstring>
 using namespace std;
 
-class table
+class Table
 {
 private:
-	map<string, vector<string>> t;
-
 
 public:
-	table(vector<string>);
-	//table(const table&) = delete;
-	//table& operator=(const table&) = delete;
+	map<string, vector<string>> t;
+	Table();
+	Table(vector<string>);
+	Table(const Table&);
+	Table& operator=(const Table&);
 	void add_row(map<string, string>);
 	void display();
-	void sel_row(string column_name, /*function pointer with bool*/); 
+	vector<string> sel_row(int row_num); 
 	void del_row(int row_num); //have to change to take predicate
 	
-	~table();
+	~Table();
 };
 
 class Context
 {
 	private:
-		map<string, vector<table>> tables ;
-		table *t;	//To store context of which table is being accessed
+		map<string, Table> tables ;
+		string table;	//To store context of which table is being accessed
 		string column;	//To store context of which column is being accessed
-		//Predicate for where
+		function<bool(string, string)> pred;//Predicate for where
 
 	public:
 		Context();
 		~Context();
-		//setTable()
-		//setColumn
-
+		void set_table(string);
+		void set_column(string);
+		void set_filter(function<bool(string, string)>);
+		void clear(); //To clear context state
+		Table get_table();
+		vector<string> get_column();
+		vector<vector<string>> search_on_filter(function<bool(string)>);
+		void delete_on_filter(function<bool(string, string)>);
 };
 
 class Expression
 {
 	virtual vector<string> interpret(Context ctx) = 0;
-}
+};
 
 class Insert : public Expression
 {
 
-}
+};
 
 class Select : public Expression
 {
 
-}
+};
 
 class From : public Expression
 {
 
-}
+};
 
 class When : public Expression
 {
 
-}
+};
 
 class Delete : public Expression
 {
 
-}
+};
 #endif
